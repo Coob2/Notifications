31939acd9efc71159431a555439dc7ac3e858802
Coob2 <COOB2_17@uni.worc.ac.uk>
2017-12-04 09:57:56 +0000

    removed empty lines

--- js/index.js
+++ js/index.js
@@ -5,33 +5,28 @@ $(document).on('pageinit', function() {
 	$('#messageButton').on('click', function() {
 		createMessage();
 	});
-	
+
 	$('#dialogButton').on('click', function() {
 		createDialog();
 	});
-
 
 	$('#notificationButton').on('click', function() {
 		createNotification();
 	});
 
-
 });
 
-
-
-function createMessage(){		
+function createMessage(){
 	//phoneGap and jQueryMobile do not support toast messages directly
     //so we can add this using toast.js
-    new Toast({content: 'An example message.', duration: 1000}); 	
+    new Toast({content: 'An example message.', duration: 1000});
 }
-        	
 
 function createDialog() {
 
 	//phonegap supports native dialog boxes.
 	//here's a simple example
-      
+
 	navigator.notification.confirm(
     	'What do you think of this dialog?',  // message
         dialogDismissed,         // callback
@@ -40,36 +35,34 @@ function createDialog() {
     );
 
 }
-        	
-        	
-        	
+
 function dialogDismissed(buttonIndex) {
-	
+
 	if(buttonIndex==1) new Toast({content: "You're easily pleased", duration: 3000});
    	else if(buttonIndex==2) new Toast({content: 'It is rather boring.', duration: 3000});
 
 }
 
-   
-   
+
+
 function createNotification() {
-        		
+
 	//
     //generate a time to post notification
     //
     var currentTime = new Date().getTime(); //current time
     var notificationTime = new Date(currentTime + 1000); //delayed time  - add 1 second
-    			
+
     //
     //setup notification
     //
-    
-    cordova.plugins.notification.local.schedule({ 
+
+    cordova.plugins.notification.local.schedule({
     	id: 		1,
         title: 		"Hey you",
         message: 	"This is an example notification",
-        date: 		notificationTime, 
+        date: 		notificationTime,
         badge: 		notification_count++
    	});
-    
+
 }
